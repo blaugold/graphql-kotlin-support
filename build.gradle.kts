2da0586c266523564aac502dbb1fc1ab671c980c
Gabriel Terwesten <gabriel@terwesten.net>
2020-10-29 11:18:15 +0100

    chore(): cut release v0.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ allprojects {
     apply(plugin = "com.diffplug.spotless")
 
     group = "com.gabrielterwesten"
-    version = "0.2.0"
+    version = "0.3.0"
 
     spotless {
         kotlinGradle {
